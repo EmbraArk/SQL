7b35cb92438b3d72592287a15736213ac9dc7641
G I TT I <121408579+EmbraArk@users.noreply.github.com>
2022-12-30 17:00:43 -0800

    Update book_db.sql

--- book_db.sql
+++ book_db.sql
@@ -35,19 +35,20 @@ WHERE author="Булгаков М.А." OR author="Есенин С.А." AND price
 
 
 /*Вывести название, автора,  цену  и количество всех книг, цена которых меньше 500 или больше 600, а стоимость всех экземпляров этих книг больше или равна 5000.*/
-/*
 
 SELECT title, author, price, amount
 FROM book
 WHERE (price < 500 OR price > 600) AND price * amount >= 5000;
 
-/*Выбрать названия и количества тех книг, количество которых от 5 до 14 включительно.
+/*Выбрать названия и количества тех книг, количество которых от 5 до 14 включительно.*/
+
 SELECT title, amount
 FROM book
-WHERE amount BETWEEN 5 AND 14;*/
+WHERE amount BETWEEN 5 AND 14;
 /*WHERE amount >= 5 AND amount <=14;*/
 
 /*Выбрать названия и цены книг, написанных Булгаковым или Достоевским.*/
+
 SELECT title, price 
 FROM book
 WHERE author IN ('Булгаков М.А.', 'Достоевский Ф.М.');
