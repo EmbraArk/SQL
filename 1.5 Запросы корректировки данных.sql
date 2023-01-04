10288591238c0cd145f84abfd766f09147d8d377
G I TT I <121408579+EmbraArk@users.noreply.github.com>
2023-01-03 18:00:23 -0800

    Update 1.5 Запросы корректировки данных.sql

--- 1.5 Запросы корректировки данных.sql
+++ 1.5 Запросы корректировки данных.sql
@@ -1,1 +1,16 @@
+Создать таблицу поставка (supply), которая имеет ту же структуру, что и таблиц book.
 
+Поле	Тип, описание
+supply_id	INT PRIMARY KEY AUTO_INCREMENT
+title	VARCHAR(50)
+author	VARCHAR(30)
+price	DECIMAL(8, 2)
+amount	INT
+
+
+CREATE TABLE supply(
+supply_id INT PRIMARY KEY AUTO-INCREMENT,
+title VARCHAR(50),
+author VARCHAR(30),
+price DECIMAL(8,2),
+amount INT);
